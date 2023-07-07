bec2bee6f1f1a7e576df4233e8c13d14378e2c14
Miguel Torres <mtorresroadby@gmail.com>
2023-07-07 10:34:43 -0400

    state var input message loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,15 @@ import { useState } from "react";
 
 export default function Home() {
 
+  const [theInput, setTheInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: "Yo, this is ChatterBot! How can I help you today?",
+    },
+  ]);
+
   return (<div>Hello there</div>)
   
   }
